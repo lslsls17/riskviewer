8fdc68d2daa5959427ae596f363519bd12f57825
lslsls17 <lswht17@gmail.com>
2021-04-17 23:23:38 -0400

    add yahoo finance

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("hello world")
+const yahooStockPrices = require('yahoo-stock-prices');
+
+
+const  testYahooPrice = async ():Promise<void> =>{
+    const data = await yahooStockPrices.getCurrentData('AAPL');
+    console.log(data); // { currency: 'USD', price: 132.05 }
+};
+
+console.log("hello world");
+testYahooPrice()
+
+
+
+
+
